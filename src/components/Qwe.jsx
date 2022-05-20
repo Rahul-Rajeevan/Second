b346ae4da0cc6f1393a72f3e6e2c8584689217c3
Rahul Rajeevan <rahul.rajeevan4@gmail.com>
2022-05-20 20:19:20 +0530

    added

---
+++ src/components/Qwe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Qwe = () => {
+  return (
+    <div>
+        <h1 className='q1'>LOGOBAKERY</h1>
+        <a href="">
+        </a><a href="">
+        </a><a href=""></a>
+        <button>Contact</button>
+    </div>
+  )
+}
+
+export default Qwe
